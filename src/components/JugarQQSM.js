fc0e452c48f7e5dbfb453b3814399b4be0bc5e67
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-19 10:47:43 +0200

    Update JugarQQSM.js

--- src/components/JugarQQSM.js
+++ src/components/JugarQQSM.js
@@ -1,26 +1,34 @@
 import { useState } from 'react';
+import Resultados from './Resultados';
 
 function JugarQQSM({preguntas}) {
 
-    console.log(preguntas)  
+    console.log(preguntas) ;
     const[index, setIndex] = useState(0);
     const[controllerSiguientePregunta, setControllerSiguientePregunta] = useState(false);
     const[preguntaMarcada, setPreguntaMarcada] = useState(-1);
+    const[handlePreguntaMarcada, setHandlePreguntaMarcada] = useState(true);
     const[preguntaCorrecta, setPreguntaCorrecta] = useState(false);
-    const[preguntaRespondida, setPreguntaRespondida] = useState(false); 
+    const[preguntaRespondida, setPreguntaRespondida] = useState(false);
+
+    const[terminarJuego, setTerminarJuego] = useState(false);
+    
+    const[preguntaAcertada, setPreguntaAcertada] = useState([]);
+    const[preguntaFallada, setPreguntaFallada] = useState([]);
+
 
     function handleSiguientePregunta(){
         let auxIndex = index + 1;
         setIndex(auxIndex);
-        setPreguntaMarcada(-1); setPreguntaCorrecta(false); setPreguntaRespondida(false);
+        setPreguntaMarcada(-1); setPreguntaCorrecta(false); setPreguntaRespondida(false); setHandlePreguntaMarcada(true);
 
-        if(index < preguntas.length){
+        if(index === (preguntas.length-2)){
             setControllerSiguientePregunta(true);
         }
 
     }
 
-    function comprobarPregunta(){   
+    function comprobarPregunta(id){   
         setPreguntaRespondida(true);    
         let respuestaCorrecta = -1; 
         for(let i=0; i<preguntas[index].respuestas.length; i++){
@@ -29,54 +37,86 @@ function JugarQQSM({preguntas}) {
         }       
         if(preguntaMarcada === respuestaCorrecta){
             setPreguntaCorrecta(true);
-        }   
+            setPreguntaAcertada([...preguntaAcertada, id]);
+            console.log("-------");
+            console.log(preguntaAcertada);
+        }else{
+            setPreguntaFallada([...preguntaFallada, id]);
+            console.log("++++++");
+            console.log(preguntaFallada);
+        }
     }
 
+    function checkPreguntaMarcada(numero){
+        setPreguntaMarcada(numero)
+        setHandlePreguntaMarcada(false);
+    }
 
+    function handleTerminarJuego(){
+        setTerminarJuego(true);
+    }
 
     return(
-    <div>
-        <p> {preguntas[index].texto}</p>
-        {preguntas[index].respuestas.map((respuesta) => (
-        (
-            respuesta.numero === preguntaMarcada
-            ?
-            (
-                preguntaRespondida === true
-                ?
-                    preguntaCorrecta === true
-                    ?
-                    <label className="px-3 py-1 rounded-lg font-bold text-black bg-green-400" >
-                        <input className='appearance-none' type='radio' name={preguntas[index].id} onChange={() => setPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
-                    </label>
-                    :
-                    <label className="px-3 py-1 rounded-lg font-bold text-black bg-red-600" >
-                        <input className='appearance-none' type='radio' name={preguntas[index].id} onChange={() => setPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
-                    </label>
-                :
-                <label className="px-3 py-1 rounded-lg font-bold text-black bg-orange-400" >
-                    <input className='appearance-none' type='radio' name={preguntas[index].id} onChange={() => setPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
-                </label>
-            )
-            :
-                <label className="px-3 py-1 rounded-lg font-bold text-black bg-slate-400" >
-                    <input className='appearance-none' type='radio' disabled={preguntaRespondida} name={preguntas[index].id} onChange={() => setPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
-                </label>
-        )
-
-        ))}
-
-        <button className="mr-2 border p-1 " onClick={() => comprobarPregunta()}>Comprobar pregunta</button>
-
+        <>
         {
-            controllerSiguientePregunta === true
+            terminarJuego === false
             ?
-                <button className="mr-2 border p-1 " onClick={() => window.location.reload()}>Terminar el juego</button>
+                <div className='w-screen h-screen bg-cover bg-no-repeat'>
+                    {console.log(preguntas)}
+                    <p> {preguntas[index].texto}</p>
+                    {preguntas[index].respuestas.map((respuesta) => (
+                    (
+                        respuesta.numero === preguntaMarcada
+                        ?
+                        (
+                            preguntaRespondida === true
+                            ?
+                                preguntaCorrecta === true
+                                ?
+                                <label className="px-3 py-1 rounded-lg font-bold text-black bg-green-400" >
+                                    <input className='appearance-none' checked={false} type='radio' name={preguntas[index].id} onChange={() => checkPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
+                                </label>
+                                :
+                                <label className="px-3 py-1 rounded-lg font-bold text-black bg-red-600" >
+                                    <input className='appearance-none' checked={false} type='radio' name={preguntas[index].id} onChange={() => checkPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
+                                </label>
+                            :
+                            <label className="px-3 py-1 rounded-lg font-bold text-black bg-orange-400" >
+                                <input className='appearance-none' type='radio' name={preguntas[index].id} onChange={() => checkPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
+                            </label>
+                        )
+                        :
+                            <label className="px-3 py-1 rounded-lg font-bold text-black bg-slate-400" >
+                                <input className='appearance-none' type='radio' disabled={preguntaRespondida} name={preguntas[index].id} onChange={() => checkPreguntaMarcada(respuesta.numero)} value={respuesta.texto} /> {respuesta.texto}
+                            </label>
+                    )
+
+                    ))}
+
+                    <button className="mr-2 border p-1 " disabled={handlePreguntaMarcada} onClick={() => comprobarPregunta(preguntas[index].id)}>Comprobar pregunta</button>
+
+                    {
+                        controllerSiguientePregunta === true
+                        ?
+                            null
+                        :
+                            <button className="mr-2 border p-1 " onClick={() => handleSiguientePregunta()}>Siguiente pregunta</button>
+                    }
+
+                    {
+                        controllerSiguientePregunta === true
+                        ?
+                            <button className="mr-2 border p-1 " onClick={() => handleTerminarJuego()}>Terminar el juego2</button>
+                        :
+                            null
+                    }
+
+                </div>
             :
-                <button className="mr-2 border p-1 " onClick={() => handleSiguientePregunta()}>Siguiente pregunta</button>
+                <Resultados preguntasCorrectas={preguntaAcertada} preguntasFalladas={preguntaFallada} ></Resultados>
         }
-        
-    </div>
+        </>
+    
         
     )
 
