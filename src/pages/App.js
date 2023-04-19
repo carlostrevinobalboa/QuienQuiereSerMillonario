d20ab83f8d950c7ec79745aac531e8ebe3aea532
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-19 10:46:10 +0200

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import AnadirPregunta from "../components/AnadirPregunta";
 
@@ -7,21 +8,21 @@ function App() {
   const [pestanha, setPestanha] = useState(0);
 
   return(
-    <div className="w-screen h-screen bg-gradient-to-b from-indigo-500 via-yellow-400 to-white flex flex-col">
-
+    <div className="w-screen h-screen bg-cover bg-no-repeat "
+    style={{ backgroundImage: "url('https://s1.ppllstatics.com/lasprovincias/www/multimedia/201912/07/media/cortadas/carlos-sobera-kHSD-U90899736077zr-1248x770@Las%20Provincias.jpg')" }}>
       {
         pestanha === 0
         ?
-        <div className="flex flex-row">
-          <button onClick={() => setPestanha(1)} className="mr-2 border p-1 ">Crear Preguntas</button>
-        </div>
+
+          <button onClick={() => setPestanha(1)} className="border">Crear Preguntas</button>
+
         :
         <AnadirPregunta></AnadirPregunta>
       }
 
     </div>
   ) 
-  
+
 }
 
 export default App;
