9fd9ad021bb875a7ba082c568eba6b2c2deebece
Carlos Treviño Balboa <48106221+carlostrevinobalboa@users.noreply.github.com>
2023-04-19 10:48:19 +0200

    Create Resultados.js

---
+++ src/components/Resultados.js
@@ -0,0 +1,14 @@
+
+
+function Resultados({preguntasCorrectas, preguntasFalladas}) {
+
+    console.log(preguntasCorrectas);
+    console.log(preguntasFalladas);
+    return(
+        <p>juego terminado</p>
+    )
+    
+
+}
+
+export default Resultados;
